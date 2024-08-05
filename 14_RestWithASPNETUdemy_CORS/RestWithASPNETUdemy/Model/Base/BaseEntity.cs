af7086ceba83cce8f1d01403a2c0670bfb070cb4
CaioHolanda <caiojholanda@gmail.com>
2024-08-05 11:42:30 +0100

    Ajustando CORS

---
+++ 14_RestWithASPNETUdemy_CORS/RestWithASPNETUdemy/Model/Base/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETUdemy.Model
+{
+    public class BaseEntity
+    {
+        [Column("id")]
+        public long Id { get; set; }
+    }
+}
---
+++ 14_RestWithASPNETUdemy_CORS/RestWithASPNETUdemy/Model/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETUdemy.Model
+{
+    [Table("books")]
+    public class Book:BaseEntity
+    {
+        [Column("author")]
+        public string Author { get; set; }
+        [Column("launch_date")]
+        public DateTime LaunchDate { get; set; }
+        [Column("price")]
+        public decimal Price { get; set; }
+        [Column("title")]
+        public string Title { get; set; }
+    }
+}
